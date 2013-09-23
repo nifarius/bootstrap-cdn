a908ffd9618d432a9b77b51f8c6baf2b458412b5
Nifarius <nifarius@gmail.com>
2013-09-23 09:32:26 +0400

    Initial commit

---
+++ lib/bootstrap/cdn.rb
@@ -0,0 +1,7 @@
+require "bootstrap/cdn/version"
+
+module Bootstrap
+  module Cdn
+    # Your code goes here...
+  end
+end
---
+++ lib/bootstrap/cdn/version.rb
@@ -0,0 +1,5 @@
+module Bootstrap
+  module Cdn
+    VERSION = "0.0.1"
+  end
+end
