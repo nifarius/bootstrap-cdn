1935a9860d8449bc84f2709cc9060c2e627c5daf
Nifarius <nifarius@gmail.com>
2014-01-16 12:53:28 +0400

    Update to bootstrap 3.0.3

--- lib/bootstrap/cdn.rb
+++ lib/bootstrap/cdn.rb
@@ -5,11 +5,11 @@ require 'bootstrap/cdn/railtie'
 
 module Bootstrap
   module CDN
-    BOOTSTRAP_VERSION = "3.0.0"
+    BOOTSTRAP_VERSION = "3.0.3"
     AWESOME_FONT_VERSION = "3.2.1"
     OFFLINE = (Rails.env.development? or Rails.env.test?)
     URL = {
-        stylesheet: "//netdna.bootstrapcdn.com/bootstrap/#{BOOTSTRAP_VERSION}/css/bootstrap.no-icons.min.css",
+        stylesheet: "//netdna.bootstrapcdn.com/bootstrap/#{BOOTSTRAP_VERSION}/css/bootstrap.min.css",
         javascript: "//netdna.bootstrapcdn.com/bootstrap/#{BOOTSTRAP_VERSION}/js/bootstrap.min.js",
         font: "//netdna.bootstrapcdn.com/font-awesome/#{AWESOME_FONT_VERSION}/css/font-awesome.min.css"
     }
